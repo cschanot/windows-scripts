0fef88bee46d8268f5f349b5bae89a461a14f062
Chris Schanot <47960200+cschanot@users.noreply.github.com>
2022-11-13 22:57:07 -0500

    Update setup_wsl.ps1

--- setup_wsl.ps1
+++ setup_wsl.ps1
@@ -9,7 +9,7 @@ if ((Test-Admin) -eq $false)  {
         # tried to elevate, did not work, aborting
     } else {
         
-        Start-Process -NoNewWindow powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "setup_wsl.ps1" -elevated' -f ($myinvocation.MyCommand.Definition))
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "setup_wsl.ps1" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
     exit
 }
