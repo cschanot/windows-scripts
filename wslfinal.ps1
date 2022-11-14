0f5ca4bce5a22cc2383eebd2e185894c1154ac85
Chris Schanot <47960200+cschanot@users.noreply.github.com>
2022-11-13 21:27:49 -0500

    Create wslfinal.ps1

---
+++ wslfinal.ps1
@@ -0,0 +1,36 @@
+Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+$isRunning = wsl.exe -u root service docker status
+# WILL BE UNRECOGNIZED IF NOT INSTALLED
+if ($isRunning -like "*unrecognized*") {
+### INSTALL AND RUN DOCKER
+wsl  curl -s https://get.docker.com/ `| bash
+wsl -u root service docker start
+
+}
+
+## IF DOCKER ALREADY INSTALLED BUT NOT RUNNING RUN IT
+if ($isRunning -like "*not*") {
+wsl -u root service docker start
+# wsl docker run --detach --name watchtower --volume /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+}
+else {
+wsl docker ps
+wsl -u root docker run --detach --name watchtower --volume /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+}
+exit
